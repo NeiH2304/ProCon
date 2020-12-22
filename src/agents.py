665a712c13547f09b6be205769155ca07ab53b58
NeiH2304 <dchy2000@gmail.com>
2020-12-22 22:34:44 +0700

    update

--- src/agents.py
+++ src/agents.py
@@ -123,14 +123,14 @@ class Agent():
                 _nags = flatten(self.get_agent_state(npos[i][agent]))
                 action_3 = torch.cat((action_3, self.actor.forward(
                     torch.from_numpy(np.array([flatten(s[i])], dtype = np.float32)).to(self.device),
-                    torch.from_numpy(np.array([flatten(_ags)], dtype = np.float32)).to(self.device))), dim=1)
+                    torch.from_numpy(np.array([flatten(_ags)], dtype = np.float32)).to(self.device))))
                 
                 action_1 = torch.cat((action_1, self.target_actor.forward(
                     torch.from_numpy(np.array([flatten(s[i])], dtype = np.float32)).to(self.device),
-                    torch.from_numpy(np.array([flatten(_ags)], dtype = np.float32)).to(self.device))), dim=1)
+                    torch.from_numpy(np.array([flatten(_ags)], dtype = np.float32)).to(self.device))))
                 action_2 = torch.cat((action_2, self.target_actor.forward(
                     torch.from_numpy(np.array([flatten(ns[i])], dtype = np.float32)).to(self.device),
-                    torch.from_numpy(np.array([flatten(_nags)], dtype = np.float32)).to(self.device))), dim=1)
+                    torch.from_numpy(np.array([flatten(_nags)], dtype = np.float32)).to(self.device))))
             ags = torch.cat((ags, action_1))
             nags = torch.cat((nags, action_2))
             _a = torch.cat((_a, action_3))
